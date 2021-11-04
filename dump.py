d8cc74997cbfdb207fb313a357fb289f11d48f0f
ginger <gtsueng@gmail.com>
2021-11-04 12:35:11 -0700

    With help from zcqian, switch to using the HTTP base dumper since the git base dumper is not useful for other plugins

--- dump.py
+++ dump.py
@@ -1,4 +1,7 @@
 import os
+import requests
+from urllib.parse import urljoin
+from biothings.hub.dataload.dumper import HTTPDumper
 
 import biothings, config
 biothings.config_for_app(config)
@@ -7,25 +10,37 @@ from config import DATA_ARCHIVE_ROOT
 import biothings.hub.dataload.dumper
 
 
-class LSTDumper(biothings.hub.dataload.dumper.GitDumper):
-
+class LSTDumper(HTTPDumper):
     SRC_NAME = "covid19_LST_reports"
     __metadata__ = {
         "src_meta": {
-            "author":{
+            "author": {
                 "name": "Ginger Tsueng",
                 "url": "https://github.com/gtsueng"
             },
-            "code":{
+            "code": {
                 "branch": "main",
-                "repo": "https://github.com/outbreak-info/covid19_LST_reports"
+                "repo": "https://github.com/outbreak-info/covid19_LST_reports.git"
             },
             "url": "https://www.covid19lst.org/",
             "license": "http://creativecommons.org/licenses/by-nc-sa/4.0/"
         }
     }
-    GIT_REPO_URL = 'https://github.com/outbreak-info/covid19_LST_report_data/tree/main/json/'
-    DEFAULT_BRANCH = 'main'
+    _JSON_URL_BASE = 'https://raw.githubusercontent.com/outbreak-info/covid19_LST_report_data/main/json/'
+
+    def create_todump_list(self, force=False, **kwargs):
+        r = requests.get(
+            'https://raw.githubusercontent.com/outbreak-info/covid19_LST_report_data/main/reportlist.txt')
+        reportlist = r.text.split('\n')
+        for list_item in reportlist:
+            remote_path = urljoin(self._JSON_URL_BASE, list_item)
+            self.to_dump.append(
+                {
+                    'remote': remote_path,
+                    'local': list_item
+                }
+            )
+        self.release = "2021.08"  # set to some sane value
     
     # override in subclass accordingly
     SRC_ROOT_FOLDER = os.path.join(DATA_ARCHIVE_ROOT, SRC_NAME)
